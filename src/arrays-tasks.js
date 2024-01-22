2932e2f9da18ebbb06307df2607faa7f03a47d4c
letanatol <letanatol@gmail.com>
2024-01-23 00:49:29 +0300

    feat: add solution task 10 insertItem

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -173,6 +173,7 @@ function isValueEqualsIndex(arr) {
 
 /**
  * Inserts the item into specified array at specified index.
+ * Вставляет элемент в указанный массив по заданному индексу.
  *
  * @param {array} arr - The input array.
  * @param {any} item - The item to insert.
@@ -182,8 +183,8 @@ function isValueEqualsIndex(arr) {
  *    insertItem([ 1, 3, 4, 5 ], 2, 1)  => [ 1, 2, 3, 4, 5 ]
  *    insertItem([ 1, 'b', 'c'], 'x', 0) => [ 'x', 1, 'b', 'c' ]
  */
-function insertItem(/* arr, item, index */) {
-  throw new Error('Not implemented');
+function insertItem(arr, item, index) {
+  return arr.splice(index, 0, item);
 }
 
 /**
