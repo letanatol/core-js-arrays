fedd4471e175786dba9930892d1530e850546389
letanatol <letanatol@gmail.com>
2024-01-23 00:05:13 +0300

    feat: add solution task 04 findAllOccurrences

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -66,6 +66,7 @@ function findElement(arr, value) {
 
 /**
  * Returns a number of all occurrences of the specified item in an array.
+ * Возвращает ряд всех вхождений указанного элемента в массив.
  *
  * @param {array} arr - The input array.
  * @param {any} item - Element to search.
@@ -78,8 +79,8 @@ function findElement(arr, value) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  return arr.filter((number) => number === item).length;
 }
 
 /**
