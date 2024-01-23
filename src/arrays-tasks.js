239a04f92888b0217cd1fb428b1e5d67e75e0b2d
letanatol <letanatol@gmail.com>
2024-01-24 01:30:45 +0300

    feat: add solution task 30 propagateItemsByPositionIndex

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -548,6 +548,8 @@ function findLongestIncreasingSubsequence(/* nums */) {
 /**
  * Propagates every item in sequence its position times
  * Returns an array that consists of: one first item, two second items, three third items etc.
+ * Распространение каждого элемента в последовательности его время положения
+ * Возвращает массив, состоящий из: одного первого элемента, двух вторых элементов, трех третьих элементов и т.д.
  *
  * @param {array} arr - The input array
  * @return {array}
@@ -559,8 +561,10 @@ function findLongestIncreasingSubsequence(/* nums */) {
  *  propagateItemsByPositionIndex([ 'a', 'b', 'c', null ]) => [ 'a', 'b', 'b', 'c', 'c', 'c',  null, null, null, null ]
  *  propagateItemsByPositionIndex([ 1,2,3,4,5 ]) => [ 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  return arr.flatMap((item, index) =>
+    Array.from({ length: index + 1 }, () => item)
+  );
 }
 
 /**
