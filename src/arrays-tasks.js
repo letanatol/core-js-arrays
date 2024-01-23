b58a1044a9991928fbac040896944f05ac32b307
letanatol <letanatol@gmail.com>
2024-01-24 02:14:53 +0300

    feat: add solution task 31 shiftArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -570,6 +570,8 @@ function propagateItemsByPositionIndex(arr) {
 /**
  * Shifts an array by n positions. If n is negative, the array is shifted to the left;
  * if positive, it is shifted to the right.
+ * Сдвигает массив на n позиций. Если n отрицательное, массив сдвигается влево;
+ * если положительная, сдвигается вправо.
  *
  * @param {array} arr - The array to be shifted.
  * @param {number} n - The number of positions to shift the array elements.
@@ -580,8 +582,10 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  const arrOne = arr.slice(-n);
+  const arrTwo = arr.slice(0, -n);
+  return arrOne.concat(arrTwo);
 }
 
 /**
